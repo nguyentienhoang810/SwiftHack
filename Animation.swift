dca570806e3cfda982cbcb6b8007e3ad393c90cf
Nguyen Tien Hoang <nguyentienhoang810@gmail.com>
2018-10-11 08:56:05 +0700

    Update Animation.swift

--- Animation.swift
+++ Animation.swift
@@ -21,3 +21,30 @@ extension UIView {
         })
     }
 }
+
+
+extension UIView {
+
+func addShimmeAnimation() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.white.cgColor, UIColor.clear.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.7, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.8)
+        gradientLayer.frame = self.bounds
+        self.layer.mask = gradientLayer
+        
+        let animation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.duration = 1.5
+        animation.fromValue = -self.frame.size.width
+        animation.toValue = self.frame.size.width
+        animation.repeatCount = .infinity
+        
+        gradientLayer.add(animation, forKey: "kevin1")
+    }
+    
+    func removeShimmeAnimation() {
+        self.layer.removeAllAnimations()
+        self.layer.mask = nil
+    }
+
+}
