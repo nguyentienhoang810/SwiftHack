0312530b89ae6d1ad03bc6e6e9b65ab13fcd4dc7
Nguyen Tien Hoang <nguyentienhoang810@gmail.com>
2018-10-11 08:50:38 +0700

    Create Animation.swift

---
+++ Animation.swift
@@ -0,0 +1,23 @@
+
+extension UIView {
+    func startScaleAnimation() {
+        self.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.alpha = 0.0
+        UIView.animate(withDuration: 0.25, animations: {
+            self.alpha = 1.0
+            self.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        })
+    }
+    
+    func removeScaleAnimation() {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.alpha = 0.0
+        }, completion:{(finished : Bool)  in
+            if (finished)
+            {
+                self.removeFromSuperview()
+            }
+        })
+    }
+}
