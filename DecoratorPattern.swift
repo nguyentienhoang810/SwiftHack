5a44612fb6ea05214d65be92ff838ca167604d4d
Nguyen Tien Hoang <nguyentienhoang810@gmail.com>
2018-12-17 15:31:54 +0700

    wrapper around our existing entity
    The best approach is to use The Decorator Pattern which acts as a wrapper around our existing entity, and implementing the additional properties and methods within it. We can provide the same interface as the underlying entity and call through to it.

--- DecoratorPattern.swift
+++ DecoratorPattern.swift
@@ -1,1 +1,45 @@
 
+protocol Animal {
+  init()
+  var name: String { get set }
+}
+
+enum Size {
+  case small, medium, large
+}
+
+struct Dog: Animal {
+  var name: String = "hachiko"
+}
+
+protocol Sized {
+  var size: Size { get set }
+}
+
+struct StaticSizedAnimal<T: Dog> : Sized {
+  var size: Size
+  private var _dog: T = T()
+  var name: String {
+    get {
+      return _dog.name
+    }
+    set {
+      _dog.name = newValue
+    }
+  }
+  
+  init(size: Size) {
+    self.size = size
+  }
+}
+
+extension StaticSizedAnimal:  CustomStringConvertible { 
+  var des: String {
+    return "\(name), \(size)"
+  }
+}
+
+let dog1 = StaticSizedAnimal<Dog>(size: .small)
+print(dog1)
+
+//hachiko small
